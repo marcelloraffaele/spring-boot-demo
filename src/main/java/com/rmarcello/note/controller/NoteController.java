90f853ac576dea7a4deda33b007cf5e45e05bba1
Raffaele Marcello <rmarcello@microsoft.com>
2025-02-04 11:21:21 +0100

    fix cross origin

--- src/main/java/com/rmarcello/note/controller/NoteController.java
+++ src/main/java/com/rmarcello/note/controller/NoteController.java
@@ -1,6 +1,7 @@
 package com.rmarcello.note.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import com.rmarcello.note.service.NoteService;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/notes")
 public class NoteController {
